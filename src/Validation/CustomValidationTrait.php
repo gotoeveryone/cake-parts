ec086d231671c4faf3cad5ca18133540a78db55a
Kazuki_Kamizuru <k.amago0721@gmail.com>
2017-10-01 15:25:48 +0900

    Validationクラス組み込み用にstaticに変更

--- src/Validation/CustomValidationTrait.php
+++ src/Validation/CustomValidationTrait.php
@@ -4,15 +4,17 @@ namespace Gotoeveryone\Validation;
 
 /**
  * カスタムのバリデーション
+ * バリデーションクラスに組み込んで利用します。
  */
 trait CustomValidationTrait
 {
     /**
      * マルチバイト文字への対応
      *
+     * @static
      * @param $check
      */
-    public function alphaNumeric($check)
+    public static function alphaNumeric($check)
     {
         return (bool) preg_match('/^[a-zA-Z0-9\(\)\'\-\s]+$/', $check);
     }
